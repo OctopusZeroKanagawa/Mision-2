7670c8a5a4f6094b1889674d943ef70ecf198d78
Andrés Jiménez <mariocupcakes31@gmail.com>
2025-06-28 09:30:33 -0500

    Link Render

--- src/components/Despliegue.jsx
+++ src/components/Despliegue.jsx
@@ -2,7 +2,7 @@ export default function Despliegue() {
     return (
       <main>
         <h2>Despliegue del proyecto</h2>
-        <a href="https://backlog-gaming.onrender.com" target="_blank">Link en Render</a>
+        <a href="https://mision-2.onrender.com" target="_blank">Link en Render</a>
       </main>
     );
   }
